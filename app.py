6bdd4dded45a65876ac8a570c36e531fe8319d53
not2nder <brtiodark@gmail.com>
2024-12-13 18:56:44 -0300

    new csv

--- app.py
+++ app.py
@@ -1,25 +1,44 @@
 import pandas as pd
 import streamlit as st
 
-df = pd.read_csv('data.csv',encoding='utf-8')
-df= df[df['PALAVRA'].notna() & (df['PALAVRA'] != '')]
+
+data = {
+    "Comunicação e Linguagem": "data/comunicação.csv",
+    "Sentimentos e Emoções": "data/sentimentos.csv",
+    "Comportamentos Humanos": "data/comportamentos.csv",
+}
 
 st.title("Dicionário")
 
-palavra = st.selectbox(
-    label='Pesquise por uma Palavra',
-    placeholder='Digite',
-    options=df.sort_values('PALAVRA')
+categorias= st.multiselect(
+    label="Comece Selecionando uma Categoria:",
+    placeholder="Digite Algo",
+    options=data
 )
 
-if palavra:
-    linha = df[df['PALAVRA']==palavra].iloc[0]
+if categorias:
+    li = []
+    for categoria in categorias:
+        df = pd.read_csv(data[categoria],encoding='utf-8')
+        li.append(df)
 
-    st.subheader(linha['PALAVRA'].upper())
-    st.markdown(f":blue-background[Significado:] {linha['SIGNIFICADO']}")
-    st.markdown(f":blue-background[Exemplo:] {linha['EXEMPLO']}")
+    df = pd.concat(li,axis=0,ignore_index=True)
+    df = df[df['PALAVRA'].notna() & (df['PALAVRA'] != '')]
 
-    st.write(f"Sinônimos de {palavra}:")
-    for significado in linha['SINÔNIMO'].split(','):
-        st.markdown(f'- {significado.title()}')
-    
+    palavra = st.selectbox(
+        label='Pesquise por uma Palavra',
+        placeholder='Digite Algo',
+        options=df.sort_values('PALAVRA')
+    )
+
+    if palavra:
+        linha = df[df['PALAVRA']==palavra].iloc[0]
+
+        st.subheader(linha['PALAVRA'].upper())
+        st.markdown(f":blue-background[Significado:] {linha['SIGNIFICADO']}")
+        st.markdown(f":blue-background[Exemplo:] {linha['EXEMPLO']}")
+
+        st.write(f"Sinônimos de {palavra}:")
+        for significado in linha['SINÔNIMO'].split(','):
+            st.markdown(f'- {significado.title()}')
+        
