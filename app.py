87909e73c081cc2d68d0a1cc6f4c6121392d8188
not2nder <brtiodark@gmail.com>
2024-12-13 17:44:35 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import streamlit as st
+
+df = pd.read_csv('data.csv',encoding='utf-8')
+df= df[df['PALAVRA'].notna() & (df['PALAVRA'] != '')]
+
+st.title("Dicionário")
+
+palavra = st.selectbox(
+    label='Pesquise por uma Palavra',
+    placeholder='Digite',
+    options=df['PALAVRA'].unique()
+)
+
+if palavra:
+    linha = df[df['PALAVRA']==palavra].iloc[0]
+
+    st.subheader(linha['PALAVRA'])
+    st.markdown(f":blue-background[Significado:] {linha['SIGNIFICADO']}")
+    st.markdown(f":blue-background[Exemplo:] {linha['EXEMPLO']}")
+
+    st.write(f"Sinônimos de {palavra}:")
+    for significado in linha['SINÔNIMO'].split(','):
+        st.markdown(f'- {significado}')
+    
