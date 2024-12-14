180a336bad6c65beb05bbc9136920dbf93a13f6f
not2nder <brtiodark@gmail.com>
2024-12-14 14:14:48 -0300

    radio

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
 import pandas as pd
 import streamlit as st
 
-
 df = pd.read_csv("data/PALAVRAS.csv",encoding='utf-8')
 
 st.title("Dicionário")
 
+grupo = st.radio(
+    "Agrupar por: ",
+    ["Tipo","Categoria"],
+    horizontal=True
+).upper()
+
 categoria= st.selectbox(
     label="Comece Selecionando uma Categoria:",
     placeholder="Digite Algo",
-    options= df[df['CATEGORIA'].notna()]['CATEGORIA'].unique()
+    options= df[df[grupo].notna()][grupo].unique()
 )
 
 if categoria:
     df_palavras = df[(df['PALAVRA'].notna()) & (df['SIGNIFICADO'].notna())]
-    palavras_validas = df_palavras[df_palavras['CATEGORIA'] == categoria]['PALAVRA'].sort_values()
+    palavras_validas = df_palavras[df_palavras[grupo] == categoria]['PALAVRA'].sort_values()
 
     palavra = st.selectbox(
         label='Pesquise por uma Palavra',
@@ -26,15 +31,18 @@ if categoria:
         st.markdown("<style>img {border-radius:10px;}</style>", unsafe_allow_html=True)
 
         linha = df_palavras[df_palavras['PALAVRA']==palavra].iloc[0]
-
+        palavra = str(linha['PALAVRA'])
         tipo = str(linha['TIPO']) if pd.notna(linha['TIPO']) else ""
 
-        st.markdown(f"#### {linha['PALAVRA'].upper()} {'- '+f'_{tipo}_' if tipo!='' else ''}")
-        st.markdown(f":blue-background[Significado:] {linha['SIGNIFICADO']}")
-        st.markdown(f":grey-background[Exemplo:] {linha['EXEMPLO']}")
+        st.markdown(f"#### {palavra.upper()} {'- '+f'_{tipo}_' if tipo!='' else ''}")
+        st.markdown(f":blue-background[**Significado:**] {linha['SIGNIFICADO']}")
+        st.markdown(f":grey-background[**Exemplo:**] {linha['EXEMPLO']}")
 
         if pd.notna(linha['IMAGEM']):
             st.image(linha['IMAGEM'], use_container_width=True)
+        if pd.notna(linha['DERIVADAS']):
+            st.markdown(f"**:grey-background[Palavras Derivadas de {palavra}]** : *{linha['DERIVADAS']}*")
+        
 
         # Exibir sinônimos, se disponíveis
         if pd.notna(linha['SINÔNIMO']):
