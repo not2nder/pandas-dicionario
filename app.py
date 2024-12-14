fbf2f5d490405910fbca2e5e1935c74a9de2227f
not2nder <brtiodark@gmail.com>
2024-12-14 14:59:53 -0300

    não agrupar

--- app.py
+++ app.py
@@ -7,41 +7,43 @@ st.title("Dicionário")
 
 grupo = st.radio(
     "Agrupar por: ",
-    ["Tipo","Categoria"],
+    ["Categoria","Tipo","Não Agrupar"],
     horizontal=True
 ).upper()
 
-categoria= st.selectbox(
-    label="Comece Selecionando uma Categoria:",
-    placeholder="Digite Algo",
-    options= df[df[grupo].notna()][grupo].unique()
+df_palavras = df[(df['PALAVRA'].notna()) & (df['SIGNIFICADO'].notna())]
+
+if grupo != "NÃO AGRUPAR":
+    categoria= st.selectbox(
+        label="Selecione uma Categoria:",
+        placeholder="Digite Algo",
+        options= df[df[grupo].notna()][grupo].unique()
+    )
+    palavras_validas = df_palavras[df_palavras[grupo] == categoria]['PALAVRA'].sort_values()
+else:
+    palavras_validas = df_palavras
+
+palavra = st.selectbox(
+    label='Pesquise por uma Palavra',
+    placeholder='Digite Algo',
+    options=palavras_validas
 )
 
-if categoria:
-    df_palavras = df[(df['PALAVRA'].notna()) & (df['SIGNIFICADO'].notna())]
-    palavras_validas = df_palavras[df_palavras[grupo] == categoria]['PALAVRA'].sort_values()
+if palavra:
+    st.markdown("<style>img {border-radius:10px;}</style>", unsafe_allow_html=True)
 
-    palavra = st.selectbox(
-        label='Pesquise por uma Palavra',
-        placeholder='Digite Algo',
-        options=palavras_validas
-    )
+    linha = df_palavras[df_palavras['PALAVRA']==palavra].iloc[0]
+    palavra = str(linha['PALAVRA'])
+    tipo = str(linha['TIPO']) if pd.notna(linha['TIPO']) else ""
 
-    if palavra:
-        st.markdown("<style>img {border-radius:10px;}</style>", unsafe_allow_html=True)
+    st.markdown(f"#### {palavra.upper()} {'- '+f'_{tipo}_' if tipo!='' else ''}")
+    st.markdown(f":blue-background[**Significado:**] {linha['SIGNIFICADO']}")
+    st.markdown(f":grey-background[**Exemplo:**] {linha['EXEMPLO']}")
 
-        linha = df_palavras[df_palavras['PALAVRA']==palavra].iloc[0]
-        palavra = str(linha['PALAVRA'])
-        tipo = str(linha['TIPO']) if pd.notna(linha['TIPO']) else ""
+    if pd.notna(linha['IMAGEM']):
+        st.image(linha['IMAGEM'], use_container_width=True)
+    if pd.notna(linha['DERIVADAS']):
+        st.markdown(f"**:grey-background[Palavras Derivadas de {palavra}]** : *{linha['DERIVADAS']}*")
 
-        st.markdown(f"#### {palavra.upper()} {'- '+f'_{tipo}_' if tipo!='' else ''}")
-        st.markdown(f":blue-background[**Significado:**] {linha['SIGNIFICADO']}")
-        st.markdown(f":grey-background[**Exemplo:**] {linha['EXEMPLO']}")
-
-        if pd.notna(linha['IMAGEM']):
-            st.image(linha['IMAGEM'], use_container_width=True)
-        if pd.notna(linha['DERIVADAS']):
-            st.markdown(f"**:grey-background[Palavras Derivadas de {palavra}]** : *{linha['DERIVADAS']}*")
-
-        if pd.notna(linha['SINÔNIMO']):
-            st.markdown(f"**Sinônimos de {palavra}:** {' '.join(f':grey-background[{i}]' for i in linha['SINÔNIMO'].split(','))}")
+    if pd.notna(linha['SINÔNIMO']):
+        st.markdown(f"**Sinônimos de {palavra}:** {' '.join(f':grey-background[{i}]' for i in linha['SINÔNIMO'].split(','))}")
